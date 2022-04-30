11801797ad74cdf2da3dd67e5b2a8cbc18d8292a
Rgarris <rgarris1298@gmail.com>
2022-04-30 12:01:07 -0600

    updated combined model

--- combined_omics_decision_tree.R
+++ combined_omics_decision_tree.R
@@ -23,8 +23,8 @@ base_dir = "C:\\Users\\rgarr\\Documents\\poly-omics-risk\\"
 # install.packages("MixRF")
 # install.packages("caret")
 # install.packages("randomForest")
-# install.packages("glue")
-
+# install.packages("glue") 
+# install.packages("sjPlot")
 library(data.table)
 library(ggplot2)
 library(R.utils)
@@ -33,6 +33,7 @@ library(UpSetR)
 library(cowplot)
 library(biomformat)
 library(compositions)
+
 library(bestglm)
 library(MASS)
 library(tree)
@@ -44,6 +45,10 @@ library(MixRF)
 library(caret)
 library(randomForest)
 library(glue)
+library(fmsb)
+library(sjPlot)
+library(sjmisc)
+library(sjlabelled)
 `%ni%` <- Negate(`%in%`)
 
 
@@ -72,41 +77,42 @@ metadata
 
 # viromics
 # setwd(glue("{base_dir}viromics"))
-viromic_MixRF <- fread("./viromics/viromics_MixRF_scores.txt", header=T)
-viromic_MixRF <- as.data.frame(viromic_MixRF[,c("External_ID","predicted")])
-viromic_MixRF <- viromic_MixRF %>% rename("virome_pred" ="predicted")
-viromic_MixRF
+viromic_glmer <- fread("./viromics/viromics_features_scores.txt", header=T)
+viromic_glmer <- as.data.frame(viromic_glmer[,c("External_ID","predicted")])
+viromic_glmer <- viromic_glmer %>% rename("virome_pred" ="predicted")
+viromic_glmer
 
 # metagenomics
 # setwd(glue("{base_dir}metagenomics"))
-metagenomic_MixRF <- fread("./metagenomics/metagenomics_MixRF_scores.txt", header=T)
-metagenomic_MixRF <- as.data.frame(metagenomic_MixRF[,c("External_ID","predicted")])
-metagenomic_MixRF <- metagenomic_MixRF %>% rename("metagen_pred" ="predicted")
-metagenomic_MixRF
+metagenomic_glmer <- fread("./metagenomics/metatragenomics_features_scores.txt", header=T)
+metagenomic_glmer <- as.data.frame(metagenomic_glmer[,c("External_ID","predicted")])
+metagenomic_glmer <- metagenomic_glmer %>% rename("metagen_pred" = "predicted")
+metagenomic_glmer
 
 # metatranscriptomics
 # setwd(glue("{base_dir}metatranscriptomics"))
-metatranscriptomic_MixRF <- fread("./metatranscriptomics/metatranscriptomics_MixRF_scores.txt", header=T)
-metatranscriptomic_MixRF <- as.data.frame(metatranscriptomic_MixRF[,c("External_ID","predicted")])
-metatranscriptomic_MixRF <- metatranscriptomic_MixRF %>% rename("metatrans_pred" ="predicted")
-metatranscriptomic_MixRF
+metatranscriptomic_glmer <- fread("./metatranscriptomics/metatranscriptomics_features_scores.txt", header=T)
+metatranscriptomic_glmer <- as.data.frame(metatranscriptomic_glmer[,c("External_ID","predicted")])
+metatranscriptomic_glmer <- metatranscriptomic_glmer %>% rename("metatrans_pred" ="predicted")
+metatranscriptomic_glmer
 
 # # metabolomics
 # setwd(glue("{base_dir}metabolomics"))
-metabolomic_MixRF <- fread("./metabolomics/metabolomics_MixRF_scores.txt", header=T)
-metabolomic_MixRF <- as.data.frame(metabolomic_MixRF[,c("External_ID","predicted")])
-metabolomic_MixRF <- metabolomic_MixRF %>% rename("metabol_pred" ="predicted")
-metabolomic_MixRF
+metabolomic_glmer <- fread("./metabolomics/metabolomics_features_scores.txt", header=T)
+metabolomic_glmer <- as.data.frame(metabolomic_glmer[,c("External_ID","predicted")])
+metabolomic_glmer <- metabolomic_glmer %>% rename("metabol_pred" ="predicted")
+metabolomic_glmer
 
 
 ## merge datasets by external ID  --#################################################################################
 # get dataset with cols: extID,Diagnosis,Score1,Score2,Score3,Score4
 
 # df_list <- list(metadata,viromic_MixRF,metagenomic_MixRF,metatranscriptomic_MixRF)
-df_list <- list(metadata,viromic_MixRF,metagenomic_MixRF,metatranscriptomic_MixRF,metabolomic_MixRF)
+df_list <- list(metadata,viromic_glmer,metagenomic_glmer,metatranscriptomic_glmer,metabolomic_glmer)
 rf_scores <- df_list %>% reduce(full_join, by="External_ID")
 rf_scores <- as.data.frame(rf_scores[complete.cases(rf_scores),])
 rf_scores 
+
 
 
 ## average individual scores by participant ID --########################################################################
@@ -137,12 +143,133 @@ view(df_for_model)
 
 combomod <- glm(as.formula(paste0("diagnosis ~ .")), data = df_for_model, family = "binomial")
 
+
 combomod_sum <- summary(combomod)
 combomod_sum
 
+
+combomod_nocovar <- glm(as.formula(paste0("diagnosis ~ virome_pred + metagen_pred + metatrans_pred + metabol_pred")), data = df_for_model, family = "binomial")
+
+combomod_nocovar_sum <- summary(combomod_nocovar)
+combomod_nocovar_sum
+
+combomod_somecovar <- glm(as.formula(paste0("diagnosis ~ virome_pred + metagen_pred + metatrans_pred + metabol_pred + consent_age + sex")), data = df_for_model, family = "binomial")
+
+combomod_somecovar_sum <- summary(combomod_somecovar)
+combomod_somecovar_sum
+plot_model(combomod_somecovar, vline.color = 'gray')
+
+
+combomod_only_somecovar <- glm(as.formula(paste0("diagnosis ~ consent_age + sex")), data = df_for_model, family = "binomial")
+
+combomod_only_somecovar <- summary(combomod_only_somecovar)
+combomod_only_somecovar
+## nagelkerke R #####
+
+
+NagelkerkeR2(combomod_nocovar)
+NagelkerkeR2(combomod)
+NagelkerkeR2(combomod_somecovar)
+NagelkerkeR2(combomod_only_somecovar)
+
+
 # #---make a regression tree---#########################################################################################################################################################################
 # require(tree)
-mytree <- tree(diagnosis ~ ., data = df_for_model)
+mytree <- tree(diagnosis ~ virome_pred + metagen_pred + metatrans_pred + metabol_pred, data = df_for_model, control = tree.control(mindev =0, minsize=3, nobs=29) )
 plot(mytree)
-text(mytree, pretty = 0, cex = .8)
+text(mytree, pretty = 1, cex = .8,  digits = 1)
 
+
+tab_model(combomod_nocovar)
+plot_model(combomod_nocovar, vline.color = 'gray')
+
+
+## looking at variance for participant -################
+# setwd(base_dir)
+# metadata2 <- fread("./testing_metadata.txt", header=T)
+# metadata2 <- as.data.frame(metadata2[,c("External ID","Participant ID","diagnosis", "consent_age", "race","sex", "site_name","Antibiotics")])
+# colnames(metadata2) <- c("External_ID","Participant_ID", "diagnosis","consent_age", "race", "sex","site_name", "Antibiotics")
+# metadata2$diagnosis <- as.character(metadata2$diagnosis)
+# metadata2$diagnosis[metadata2$diagnosis == "UC"] <- 1
+# metadata2$diagnosis[metadata2$diagnosis == "CD"] <- 1
+# metadata2$diagnosis[metadata2$diagnosis == "nonIBD"] <- 0
+# metadata2$diagnosis <- as.numeric(metadata2$diagnosis)
+# metadata2$External_ID <- as.character(metadata2$External_ID)
+# metadata2$Participant_ID <- as.character(metadata2$Participant_ID)
+# metadata2$sex <- as.factor(metadata2$sex)
+# metadata2$site_name <- as.factor(metadata2$site_name)
+# metadata2$consent_age <- as.numeric(metadata2$consent_age)
+# metadata2$race <- as.factor(metadata2$race)
+# metadata2$Antibiotics <- as.factor(metadata2$Antibiotics)
+# metadata2
+# 
+# df_list <- list(metadata2,viromic_MixRF,metagenomic_MixRF,metatranscriptomic_MixRF,metabolomic_MixRF)
+# rf_scores <- df_list %>% reduce(full_join, by="External_ID")
+# rf_scores <- as.data.frame(rf_scores[complete.cases(rf_scores),])
+# rf_scores 
+# 
+# par_variance = rf_scores %>%
+#   subset(select= -c(External_ID, consent_age, race, sex, site_name))
+# 
+# hist(group_by) 
+
+par_variance <- rf_scores %>%
+  group_by(Participant_ID) %>%
+  summarize( virome_pred_avg = mean(virome_pred),
+             virome_var = var(virome_pred),
+             virome_range = max(virome_pred)-min(virome_pred),
+             #virome_min = min(virome_pred),
+             #virome_max = max(virome_pred),
+             metagen_pred_avg = mean(metagen_pred), 
+             metagen_range = max(metagen_pred)-min(metagen_pred),
+             metatrans_pred_avg = mean(metatrans_pred),
+             metatrans_var = var(metatrans_pred),
+             metatrans_range = max(metatrans_pred)-min(metatrans_pred),
+             metabol_pred_avg = mean(metabol_pred),
+             metabol_var = var(metabol_pred),
+             metabol_range = max(metabol_pred)-min(metabol_pred)
+             )
+  
+
+
+par_variance  
+
+
+hist(par_variance$virome_var)
+hist(par_variance$metagen_var)
+hist(par_variance$metatrans_var)
+hist(par_variance$metabol_var)
+
+hist(par_variance$virome_range)
+hist(par_variance$metagen_range)
+hist(par_variance$metatrans_range)
+hist(par_variance$metabol_range)
+
+
+vir_by_par <- rf_scores %>%
+  transmute(Participant_ID=Participant_ID, virome_pred=virome_pred)
+
+vir_by_par$virome_pred <- as.numeric(vir_by_par$virome_pred)
+vir_by_par$Participant_ID <- as.factor(vir_by_par$Participant_ID)
+
+
+
+# vir_by_par <- vir_by_par %>%
+#   group_by(Participant_ID) %>%
+#   hist(virome_pred)
+
+
+vir_by_par <- reshape(
+  data = vir_by_par,
+  direction = "wide",
+  idvar = "Participant_ID",
+  
+)
+
+
+
+
+
+for (par in rf_scores$Participant_ID) {
+  hist(rf_scores$virome_pred)
+}
