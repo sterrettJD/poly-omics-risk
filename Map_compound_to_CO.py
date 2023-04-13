68276e549dbafa94310cddb2350ff64aa4600c2e
John Sterrett <sterrettJD@protonmail.com>
2023-04-13 09:50:25 -0600

    convert res to DF

--- Map_compound_to_CO.py
+++ Map_compound_to_CO.py
@@ -1,10 +1,22 @@
 import requests
 import pandas as pd
+from io import StringIO
+
 def get_compound_info(compound):
     res = requests.get(f"https://rest.kegg.jp/find/compound/{compound}")
-    return res.content
+    return res.content.decode('UTF-8')
+
+
+def parse_res_to_df(info):
+    res_df = pd.read_csv(StringIO(info), sep="\t", header=None)
+    res_df.columns = ["CO", "Names"]
+    return res_df
+
+
 def main():
     info = get_compound_info("phenyllactate")
+    info = parse_res_to_df(info)
+
     print(info)
 
 
