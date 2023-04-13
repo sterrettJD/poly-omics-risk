21fe0f15f94a619896cf04ccb5568fb3110df6b1
John Sterrett <sterrettJD@protonmail.com>
2023-04-13 09:56:10 -0600

    add read input func

--- Map_compound_to_CO.py
+++ Map_compound_to_CO.py
@@ -2,6 +2,12 @@ import requests
 import pandas as pd
 from io import StringIO
 
+
+def read_input_file(fp):
+    with open(fp, "r") as file:
+        file_contents = file.readline().split("\t")
+    return file_contents
+
 def get_compound_info(compound):
     res = requests.get(f"https://rest.kegg.jp/find/compound/{compound}")
     return res.content.decode('UTF-8')
@@ -14,7 +20,14 @@ def parse_res_to_df(info):
 
 
 def main():
+    compounds = read_input_file("selected_metabolites.txt")
+    print(compounds)
+
     info = get_compound_info("phenyllactate")
+
+    if info is None:
+        return
+
     info = parse_res_to_df(info)
 
     print(info)
